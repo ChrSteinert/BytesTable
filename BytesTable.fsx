87f6cdab3a58421ba2701499d0dd9ba535993288
Christian Steinert <chr.steinert@outlook.com>
2019-06-16 11:01:00 +0200

    Add files via upload

---
+++ BytesTable.fsx
@@ -0,0 +1,31 @@
+let base64 = "yNOjBk3eyPczM4pMCABFAAA3o9kAAIARFRTAqAB3wKgAAcoeADUAI0yXvUgBAAABAAAAAAAABmdvb2dsZQJkZQAAAQAB"
+let bytes = System.Convert.FromBase64String base64
+
+let getBytesTable (bytes : byte array) = 
+    let sb = System.Text.StringBuilder ()
+    bytes 
+    |> Array.iteri (fun i c -> 
+        if i % 16 = 0 then 
+            if i > 0 then sb.AppendLine () |> ignore
+            sb.AppendFormat("{0:x4}\t", i) |> ignore
+
+
+        sb.AppendFormat("{0:x2}", c) |> ignore
+        if i % 16 = 8 then sb.Append ' ' |> ignore
+        elif i % 16 = 15 then
+            sb.Append "    " |> ignore
+            for i = i - 15 to i do
+                if bytes.[i] >= 32uy && bytes.[i] <= 126uy 
+                then bytes.[i] |> char
+                else '\183' 
+                |> sb.Append
+                |> ignore
+        if i + 1 < bytes.Length then sb.Append ' ' |> ignore
+    )
+
+    sb.ToString ()    
+
+bytes |> getBytesTable
+
+fsi.AddPrinter getBytesTable
+bytes
